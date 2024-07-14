6e0ac505f1f8efca3131c253581cd961531566b3
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-07-14 19:53:30 +0530

    image.model.ts : create image model file along with the interface IImage to be exported to the frontend

--- README.md
+++ README.md
@@ -24,5 +24,12 @@ pnpm run dev
 
 The project should be up and running at : `http://localhost:3000`
 
+## What I learned/did while working on this project?
+
+1. Creating a svg for the app using AI(ChatGPT and DALL&bull;E)
+2. Creating navbars specific to mobile and desktop views
+3. Using ShadCn UI library and it's button and sheet components
+4. Caching mongodb connections to optimize the performance as the Next.js app runs in a serverless enviroment.
+5. 
 
 ## Features:
---
+++ src/lib/database/models/image.model.ts
@@ -0,0 +1,43 @@
+import { Document, model, models, Schema, ObjectId } from "mongoose";
+
+const imageSchema = new Schema({
+  title: { type: "String", required: true },
+  transformationType: { type: "String", required: true },
+  publicId: { type: String, required: true },
+  sceureUrl: { type: URL, required: true },
+  width: { type: Number },
+  innerHeight: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model("Image", imageSchema);
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: URL;
+  width?: number;
+  innerHeight?: number;
+  config?: Record<string, any>;
+  transformationUrl?: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export default Image;
