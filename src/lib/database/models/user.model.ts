32bba4d062ba0f2aa990a96d0e5806224c2a8dbd
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-07-14 20:03:17 +0530

    create user model and interface to export to frontend

---
+++ src/lib/database/models/user.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: URL;
+  firstName?: string;
+  lastName?: string;
+  planId?: number;
+  password: string;
+  creditBalance?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: { type: URL, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  planId: { type: Number, default: 1 },
+  password: { type: String, required: true },
+  creditBalance: { type: Number, default: 10 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = models?.image || model("User", userSchema);
+export default User;
