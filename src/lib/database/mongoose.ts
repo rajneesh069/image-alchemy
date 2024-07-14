7c0e62fbeb4743c44f73d6b9bb7b5621ebb495af
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-07-14 19:39:19 +0530

    create mongoose connection file and ensure that the connection is cached

---
+++ src/lib/database/mongoose.ts
@@ -0,0 +1,40 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// Since Next.js runs in a serverless environment so it connects to the database each time
+//while in case of express.js it's the server itself which runs for a prolonged time
+//hence no need to connect to database again and again.
+
+// Serverless functions are stateless, i.e., they startup to handle a request and then shut down without maintaining
+// a prolonged connection with the database.
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async()=>{
+    if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URL){
+        throw new Error("Missing MONGODB_URL")
+    }
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName : "Image-Alchemy",
+        bufferCommands : false,
+    })
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
